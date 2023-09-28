a0e646067cf9076731cdd74f82a0e4a637464485
lishiyuan <lishiyuandage@gmail.com>
2023-09-28 22:15:19 +0800

    todo: 考虑缓存

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,6 +1,8 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
+
+// 也许我应该定义一个存储小国、狠人、组织的store，这样可以在本地获取数据
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
